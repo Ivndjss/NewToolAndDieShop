181e28006ca033e89cdccf3fa08e198cf6b5da7d
Liére <163969038+SoLayZ@users.noreply.github.com>
2024-03-19 13:17:14 +0800

    Update sidepanel.js
    site referencing

--- ToolsAndDieShop/Admin/sidepanel.js
+++ ToolsAndDieShop/Admin/sidepanel.js
@@ -1,20 +1,19 @@
 function createJob() {
     // Replace with your actual URL for creating a job
-    window.location.href = "https://your-website.com/create-job";
+    window.location.href = "add.html";
   }
   
   function viewJobs() {
     // Replace with your actual URL for viewing jobs
-    window.location.href = "https://your-website.com/view-jobs";
+    window.location.href = "view.html";
   }
   
   function updateJobs() {
     // Replace with your actual URL for updating jobs
-    window.location.href = "https://your-website.com/update-jobs";
+    window.location.href = "updateAndDelete";
   }
   
   function removeJobs() {
     // Replace with your actual URL for removing jobs
-    window.location.href = "https://your-website.com/remove-jobs";
+    window.location.href = "updateAndDelete";
   }
-  
